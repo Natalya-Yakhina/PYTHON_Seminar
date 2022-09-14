34e01036cc72345dcefc6f7f97073e365f9ba58a
Яхина Наталья Андреевна <  @vvrz.local>
2022-09-14 12:27:35 +0300

    017

---
+++ Task17.py
@@ -0,0 +1,17 @@
+# 17. Задайте строку из набора чисел. Напишите программу, которая покажет большее и меньшее число. 
+# В качестве символа-разделителя используйте пробел.
+
+from random import randint
+
+
+a = int(input('Введите число: '))
+list = []
+
+for i in range(a):
+    f = randint(-a, a)
+    list.append(f)
+print(list)
+# print(min(list))
+# print(max(list))
+
+print(f'Наименьшее число: {min(list)}, наибольшее число: {max(list)}')
