5604f2c7c40385dc8a1297ee48069d1a47a9c119
Яхина Наталья Андреевна <  @vvrz.local>
2022-09-14 12:18:30 +0300

    16

--- Task16.py
+++ Task16.py
@@ -1,6 +1,6 @@
 t = ['a', 'b', 'v', 'g', 'd', 'e', 'zh', 'z', 'i', 'y', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'f', 'kh', 'ts', 'ch', 'sh', 'shch', '', 'y', '', 'e', 'yu', 'ya']
 
-start_index = ord('а')
+start_index = ord('а') # ord - обращение к коду
 title = 'Переводим этот текст, сейчас!'
 print(title.lower())
 
