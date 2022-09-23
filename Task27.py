89de674edd73eafdd7a66ed5d53d9877a9229ceb
Яхина Наталья Андреевна <  @vvrz.local>
2022-09-23 13:58:25 +0300

    число в списке

--- Task27.py
+++ Task27.py
@@ -27,3 +27,25 @@ try:
 except:
     print("Такого числа нет.")
 
+
+
+# def leave_only_numbers(st):
+# rezult = ''
+
+# for i in st:
+# if ord(i) > ord('/') and ord(i) < ord(':'):
+# rezult += i
+
+# return rezult
+
+
+# number = input('Введите число: ')
+
+# n_list = ['апап4', 'fdgg3', 'fdgf', '6', 'fg24']
+
+# ind = 0
+# for i in n_list:
+# t_el = leave_only_numbers(i)
+# if number == t_el:
+# print(f'{n_list} - ищем {number} - найдено на {ind} индексе')
+# ind += 1
