999613a9c4ae1ac7f0737729da9207a437c4c617
Яхина Наталья Андреевна <  @vvrz.local>
2022-09-27 11:49:14 +0300

    сумма квадратов 2х значных чисел делящихся на 9

---
+++ Task29.py
@@ -0,0 +1,20 @@
+# 4. Напишите программу, которая подсчитает и выведет сумму квадратов всех двузначных чисел, делящихся на 9.
+# При решении задачи используйте комбинацию функций filter, map, sum.
+
+# Обратите внимание: на 9 должно делиться исходное двузначное число, а не его квадрат.
+
+
+from random import randint
+
+list1 = [i for i in range(10,100)]
+list2 = list(filter(lambda x: x % 9 == 0, list1))
+list3 = sum(map(lambda x: x**2, list2))
+
+print(list1)
+print()
+print(f'Список двухзначных чисел делящихся на 9: {list2}')
+print()
+print(f'Сумма квадратов двухзначных чисел = {list3}')
+
+
+# print(sum(map(lambda x: x**2, list(filter((lambda x: x % 9 == 0), list(range(10, 100)))))))
